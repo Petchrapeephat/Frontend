550241b3de8c8c854128998b0f0694b4b0f86a86
petchrapeephat123@gmail.com <Petchrapeephat123@gmail.com>
2025-06-16 13:03:30 +0700

    16/06/2568

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className="ftext-4xl text-center text-blue-950">Home Page</h1>
+    <h1 className="ftext-4xl text-center text-blue-950">Home Page SS</h1>
     </>
   )
 }
