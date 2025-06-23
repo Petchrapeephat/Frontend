c9a33ce5db76d5d71a23144031afaf289eddee03
petchrapeephat123@gmail.com <Petchrapeephat123@gmail.com>
2025-06-23 09:16:36 +0700

    VVVVVV

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 export default function Service() {
   return (
     <>
-    <h1 className="ftext-4xl text-center text-blue-950">Service Page</h1>
+    <h1 className="ftext-4xl text-center text-blue-950">About Page</h1>
     </>
   )
 }
---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <>
+    <h1 className="ftext-4xl text-center text-blue-950">Contact Page</h1>
+    </>
+  )
+}
